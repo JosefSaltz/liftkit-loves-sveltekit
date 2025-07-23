52efb8c73ec5e48c8f0a296c0bbd10c6a8912ef2
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-07-23 00:07:00 -0700

    refactor(material-layer): Convert off of Next CSS-In-JS styles
    This component will require serious review, a significant refactor was made to work around Next JS' CSS-in-JS and the logic is now rehoused in the script tag to be determined at runtime, declared in the render markdown dynamically, and then processed by rules outlined in the style tag. Reusing this feature may impact translation to other frameworks.

--- svelte-version/registry/sveltekit/components/material-layer/material-layer.svelte
+++ svelte-version/registry/sveltekit/components/material-layer/material-layer.svelte
@@ -27,9 +27,23 @@
   }
 
   let { zIndex = 0, type, materialProps }: LkMaterialLayerProps = $props();
-    /**If materialProps are provided, loop through the keys and pass each one as a data attribute to the component. */
-  let lkMatProps: LkMatProps = $derived(materialProps ? propsToDataAttrs(materialProps, `${type}`) : undefined);
-
+  /**If materialProps are provided, loop through the keys and pass each one as a data attribute to the component. */
+  let lkMatProps?: LkMatProps = $derived(materialProps ? propsToDataAttrs(materialProps, `${type}`) : undefined);
+  // Svelte work around for Next.js CSS-In-JS feature
+  const blurToLkSize = (blur: LkMatProps_Glass["thickness"]) => { 
+    if(blur === "thick") return '--lk-size-lg';
+    if(blur === "normal") return "--lk-size-md";
+    if(blur === "thin") return "--lk-size-xs";
+    // Exception default
+    return "--lk-size-md"
+   }
+  // Dynamic CSS-In-JS values extrapolated
+  const baseGlassOpacity = $derived(`${getGlassFillOpacity((materialProps as LkMatProps_Glass)?.thickness || "normal")}`)
+  const materialBlur = $derived(blurToLkSize((materialProps as LkMatProps_Glass)?.thickness || "normal"));
+  const materialBackgroundColorLight = $derived(`${getGlassFillOpacity((materialProps as LkMatProps_Glass)?.thickness || "normal")}`);
+  const materialBackgroundColor = $derived(`--lk-${(materialProps as LkMatProps_Glass)?.tint || "transparent"}`);
+  const materialOpacity = $derived((materialProps as LkMatProps_Glass)?.tintOpacity || 0.2);
+  const flatBgColor = $derived(`${getBgColor((materialProps as LkMatProps_Flat)?.bgColor)}`);
   /**Commented out, was likely used for debugging */
 
   // switch (material) {
@@ -61,7 +75,14 @@
   }
 </script>
 
- <div data-lk-component="material-layer" data-lk-material-type={type} style={{ zIndex: zIndex }}>
+ <div data-lk-component="material-layer" data-lk-material-type={type} style:zIndex={ zIndex } style="
+  --base-glass-opacity: {baseGlassOpacity}
+  --flat-background-color: {flatBgColor}
+  --material-blur: {materialBlur}; 
+  --material-background-light: {materialBackgroundColorLight}
+  --material-background-color: {materialBackgroundColor}; 
+  --material-opacity: {materialOpacity}
+  ">
   {#if type === "glass"}
     <div>
       <div data-lk-material-sublayer="texture">
@@ -101,35 +122,30 @@
 
   [data-lk-material-type="glass"] {
     [data-lk-material-sublayer="tint"] {
-      opacity: ${(materialProps as LkMatProps_Glass)?.tintOpacity || 0.2};
-      background-color: var(--lk-${(materialProps as LkMatProps_Glass)?.tint || "transparent"});
+      opacity: var(--material-opacity);
+      background-color: var(--material-background-color);
     }
 
     [data-lk-material-sublayer="texture"] {
-      --blur-thick: var(--lk-size-lg);
-      --blur-normal: var(--lk-size-md);
-      --blur-thin: var(--lk-size-xs);
-
       z-index: 1;
       isolation: isolate;
-      backdrop-filter: blur(var(--blur-${(materialProps as LkMatProps_Glass)?.thickness || "normal"}));
+      backdrop-filter: blur(var(--material-blur));
     }
 
     [data-lk-material-sublayer="light"] {
-      background: ${(materialProps as LkMatProps_Glass)?.lightExpression || "none"};
+      background: var(--material-background-light);
       mix-blend-mode: soft-light;
       opacity: 1;
     }
 
     [data-lk-material-sublayer="base-glass-fill"] {
       background-color: var(--lk-surface);
-      opacity: ${getGlassFillOpacity((materialProps as LkMatProps_Glass)?.thickness || "normal")};
+      opacity: var(--base-glass-opacity);
     }
   }
   [data-lk-material-type="flat"] {
     [data-lk-material-sublayer="bgColor"] {
-      background-color: ${getBgColor((materialProps as LkMatProps_Flat)?.bgColor)};
-
+      background-color: var(--flat-background-color);
     }
   }
 </style>
