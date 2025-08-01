33b15d1249715215e624caeab54074e684b42829
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-01 01:02:55 -0700

    fix(ts): Pacify Nested Generics
    Typescript was super mad about passing T as the generic type to another nested type. Creating a Variant union seems to pacify the compiler and inference appears to be working correctly now.

--- svelte-version/registry/sveltekit/components/icon-button/icon-button.svelte
+++ svelte-version/registry/sveltekit/components/icon-button/icon-button.svelte
@@ -1,73 +1,70 @@
+
+
 <script lang="ts">
-  import { useMemo } from "react";
   import { propsToDataAttrs } from "@/registry/sveltekit/lib/utilities";
   import "@/registry/sveltekit/components/icon-button/icon-button.css";
   import Icon from "@/registry/sveltekit/components/icon";
   import StateLayer from "@/registry/sveltekit/components/state-layer";
-  import { IconName } from "lucide-react/dynamic";
+  import type { IconName } from "lucide-react/dynamic";
   import { getOnToken } from "@/registry/universal/lib/colorUtils";
 	import type { HTMLAttributes } from "svelte/elements";
 
-  declare global {
-    type LkIconButtonSize = "xs" | "sm" | "md" | "lg" | "xl";
-  }
-  // TODO: Need to pick a good candidate to use as a replacement type
+
   interface LkIconButtonBaseProps extends HTMLAttributes<HTMLButtonElement> {
     icon: IconName;
     size?: LkIconButtonSize;
     fontClass?: LkFontClass; //optional, if present it will control the size directly via fontClass
     className?: string; //optional, making explicit here so we can control how it mixes in with locally-passed props
   }
+  type IconVariants = "fill" | "outline" | "text";
+  type IconButtonColor<T extends IconVariants> = T extends "fill" ? LkColorWithOnToken : LkColor;
 
-  type IconButtonColor<T extends "fill" | "outline" | "text"> = T extends "fill" ? LkColorWithOnToken : LkColor;
-
-  export interface LkIconButtonProps<T extends "fill" | "outline" | "text" = "fill"> extends LkIconButtonBaseProps {
+  interface LkIconButtonProps extends LkIconButtonBaseProps {
     icon: IconName;
-    variant?: T;
-    color?: IconButtonColor<T>;
+    variant?: IconVariants;
+    color?: IconButtonColor<IconVariants>;
   }
 
   let {
     icon = "roller-coaster",
-    variant = "fill" as IconButtonColor,
-    color = "primary" as IconButtonColor,
+    variant = "fill",
+    color = "primary",
     size = "md",
     fontClass = "body",
     className,
     ...restProps
   }: LkIconButtonProps = $props();
-    const dataAttrs = $derived(propsToDataAttrs({ variant, color, size }, "icon-button"));
+  const dataAttrs = $derived(propsToDataAttrs({ variant, color, size }, "icon-button"));
 
-    const onToken = getOnToken(color) as LkColor;
+  const onToken = getOnToken(color) as LkColor;
 
-    /** Dynamically set stroke width based on font class */
-    let iconStrokeWidth: number = getIconStrokeWidth(fontClass);
+  /** Dynamically set stroke width based on font class */
+  let iconStrokeWidth: number = getIconStrokeWidth(fontClass);
 
-    function getIconStrokeWidth(fontClass: LkFontClass) {
-      switch (fontClass) {
-        case "display1":
-        case "display2":
-        case "title1":
-          return 1.5;
-        case "subheading":
-        case "label":
-        case "caption":
-        case "capline":
-          return 2;
-        default:
-          return 1.75;
-      }
+  function getIconStrokeWidth(fontClass: LkFontClass) {
+    switch (fontClass) {
+      case "display1":
+      case "display2":
+      case "title1":
+        return 1.5;
+      case "subheading":
+      case "label":
+      case "caption":
+      case "capline":
+        return 2;
+      default:
+        return 1.75;
     }
+  }
 
-    /**Dynamically set icon and state-layer color based on variant */
-    function getIconColor(variant: "fill" | "outline" | "text") {
-      switch (variant) {
-        case "outline":
-        case "text":
-          return color;
-        case "fill":
-          return onToken;
-      }
+  /**Dynamically set icon and state-layer color based on variant */
+  function getIconColor(variant: "fill" | "outline" | "text") {
+    switch (variant) {
+      case "outline":
+      case "text":
+        return color;
+      case "fill":
+        return onToken;
     }
   }
 </script>
@@ -77,7 +74,7 @@
   type="button"
   {...dataAttrs}
   {...restProps}
-  className={`${fontClass} ${className || ""}`}
+  class={`${fontClass} ${className || ""}`}
 >
   <div>
     <Icon name={icon} color={getIconColor(variant)} strokeWidth={iconStrokeWidth}></Icon>
