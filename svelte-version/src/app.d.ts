c29e35b2e9e668be934f30a6a6c38ce38c1178da
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-04 09:45:03 -0700

    update(types): Update LkIconButtonSize into global type space

--- svelte-version/src/app.d.ts
+++ svelte-version/src/app.d.ts
@@ -2,12 +2,14 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
+	type LkIconButtonSize = "xs" | "sm" | "md" | "lg" | "xl";
 }
 
 export {};
