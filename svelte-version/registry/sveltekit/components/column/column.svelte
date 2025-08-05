9a153007696746d16ba15fa7a9e534e0286c31d6
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-04 21:29:41 -0700

    add(component): Migrate column component

---
+++ svelte-version/registry/sveltekit/components/column/column.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { propsToDataAttrs } from "@/registry/sveltekit/lib/utilities";
+  import "@/registry/sveltekit/components/column/column.css";
+	import type { HTMLAttributes } from "svelte/elements";
+	import type { Snippet } from "svelte";
+
+  interface LkColumnProps extends HTMLAttributes<HTMLDivElement> {
+    alignItems?: "start" | "center" | "end" | "stretch";
+    justifyContent?: "start" | "center" | "end" | "space-between" | "space-around";
+    gap?: LkSizeUnit | "none";
+    wrapChildren?: boolean;
+    defaultChildBehavior?: "auto-grow" | "auto-shrink" | "ignoreFlexRules" | "ignoreIntrinsicSize";
+    children?: Snippet;
+  }
+
+  let { children, alignItems="stretch", justifyContent="start", gap, wrapChildren, defaultChildBehavior, ...restProps }: LkColumnProps = $props();
+
+  const lkColumnAttrs = $derived(
+    propsToDataAttrs({ alignItems, defaultChildBehavior, justifyContent, gap, wrapChildren }, "column"),
+  );
+</script>
+
+<div {...lkColumnAttrs} {...restProps} data-lk-component="column">
+  {@render children?.()}
+</div>
+
