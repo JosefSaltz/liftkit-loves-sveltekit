4a2137e02cfd33656d042420be75b4a31c1bfe94
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-04 23:08:11 -0700

    update(placeholder-block): Can now take a count prop and generate as many blocks at the author needs

--- svelte-version/registry/sveltekit/components/placeholder-block/placeholder-block.svelte
+++ svelte-version/registry/sveltekit/components/placeholder-block/placeholder-block.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import "@/registry/sveltekit/components/placeholder-block/placeholder-block.css";
+
+  let { count: length = 0 } = $props();
 </script>
 
-<div data-lk-component="placeholder-block"></div>
+{#each Array.from({ length })}
+  <div data-lk-component="placeholder-block"></div>
+{/each}
 
