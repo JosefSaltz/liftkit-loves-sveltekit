89bcec6647ad1093452ddd7dbc7a0f1359e6ca27
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-05 00:50:26 -0700

    update(navbar): Implement snippets ported from root page

--- svelte-version/registry/sveltekit/components/navbar/navbar.svelte
+++ svelte-version/registry/sveltekit/components/navbar/navbar.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
+  import "@/registry/sveltekit/components/navbar/navbar.css";
   import { propsToDataAttrs } from "@/registry/sveltekit/lib/utilities";
   import Image from "@/registry/sveltekit/components/image";
+  import Button from "@/registry/sveltekit/components/button"
   import IconButton from "@/registry/sveltekit/components/icon-button";
   import Row from "@/registry/sveltekit/components/row";
   import Column from "@/registry/sveltekit/components/column";
-  import "@/registry/sveltekit/components/navbar/navbar.css";
   import MaterialLayer from "@/registry/sveltekit/components/material-layer";
   import PlaceholderBlock from "@/registry/sveltekit/components/placeholder-block";
 	import type { HTMLAttributes } from "svelte/elements";
-	import type { Snippet } from "svelte";
+	import { Dropdown, DropdownMenu, DropdownTrigger } from "@/registry/nextjs/components/dropdown";
 
   interface LkNavBarProps extends HTMLAttributes<HTMLDivElement> {
     material?: LkMaterial;
     // TODO: These types need to be changed to something more appropriate
-    navButtons?: Snippet[];
-    navDropdowns?: Snippet[];
-    iconButtons?: Snippet[];
-    ctaButtons?: Snippet[];
+    navButtons?: { key: string, label: string, variant: string }[];
+    navDropdowns?: { key: string, icon: string, variant: string, color: string }[];
+    navIconButtons?: { key: string, icon: string, variant: string, color: string }[];
+    ctaButtons?: { key: string, label: string, variant: string, color: string }[];
   }
 
   /**
@@ -46,33 +47,42 @@
     material = "flat",
     navButtons,
     navDropdowns,
-    iconButtons,
+    navIconButtons,
     ctaButtons,
     ...restProps
-  }: LkNavBarProps = $props()
+  }: LkNavBarProps = $props();
   const dataAttrs = $derived(propsToDataAttrs({ material, restProps }, "navbar"));
 
   let menuOpen = $state(false);
   const setMenuOpen = (state: boolean) => menuOpen = state;
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
-  // Using Svelte Templative Syntax for implementation instead
-  // Next JS Version
-  // // If no navButtons or navDropdowns are provided, render placeholder blocks
-  // function getPlaceholderBlocks(count: number) {
-  //   const blocks = [];
-  //   for (let i = 0; i < count; i++) {
-  //     blocks.push(() => <PlaceholderBlock key={i} />);
-  //   }
-  //   return blocks;
-  // }
+// Using Svelte Templative Syntax for implementation instead
+// Next JS Version
+// // If no navButtons or navDropdowns are provided, render placeholder blocks
+// function getPlaceholderBlock(count: number) {
+//   const blocks = [];
+//   for (let i = 0; i < count; i++) {
+//     blocks.push(() => <PlaceholderBlock key={i} />);
+//   }
+//   return blocks;
+// }
 
 </script>
-<!-- Snippet for generating placeholder blocks -->
-{#snippet PlaceholderBlocks(length)}
-  {#each Array.from({ length }) as _}
-    <PlaceholderBlock />
-  {/each}
+<!-- Snippets to pass as values for props -->
+{#snippet navButton(key, label, variant)}
+  <Button {key} {label} {variant} />
+{/snippet}
+
+{#snippet navIconButton(key, icon, variant, color)}
+  <IconButton {key} {icon} {variant} {color} />
+{/snippet}
+
+{#snippet ctaButton(key, label, variant, color)}
+  <Button {key} {label} {variant} {color} />
+{/snippet}
+{#snippet navDropdown(key, icon, variant, color)}
+  <!-- TODO: Some dropdown snippet rendering -->
 {/snippet}
 
 <div data-lk-component="navbar" {...dataAttrs}>
@@ -86,39 +96,39 @@
     <Row>
       <Row data-lk-slot="nav-buttons">
         {#if navButtons}
-          {#each navButtons as navButton}
-            {@render navButton()}
+          {#each navButtons as { key, label, variant }}
+            {@render navButton(key, label, variant)}
           {/each}
         {:else}
-          {@render PlaceholderBlocks(2)}
+          <PlaceholderBlock count={2} />
         {/if}
       </Row>
       <Row data-lk-slot="nav-dropdowns">
         {#if navDropdowns}
-          {#each navDropdowns as navDropdown}
-            {@render navDropdown()}
+          {#each navDropdowns as { key, icon, variant, color }}
+            {@render navDropdown(key, icon, variant, color)}
           {/each}
         {:else}
-          {@render PlaceholderBlocks(2)}
+          <PlaceholderBlock count={2} />
         {/if}
       </Row>
     </Row>
     <Row data-lk-navbar-el="nav-menu-end">
       <div data-lk-slot="nav-icon-buttons">
-        {#if iconButtons}
-          {#each iconButtons as iconButton}
-            {@render iconButton()}
+        {#if navIconButtons}
+          {#each navIconButtons as { key, icon, variant, color }}
+            {@render navIconButton(key, icon, variant, color)}
           {/each}
         {:else}
-          {@render PlaceholderBlocks(3)}
+          <PlaceholderBlock count={3} />
         {/if}</div>
       <div data-lk-slot="nav-cta-buttons">
         {#if ctaButtons}
-          {#each ctaButtons as ctaButton}
-            {@render ctaButton()}
+          {#each ctaButtons as { key, label, variant, color }}
+            {@render ctaButton(key, label, variant, color)}
           {/each}
         {:else}
-          {@render PlaceholderBlocks(2)}
+          <PlaceholderBlock count={2} />
         {/if}</div>
     </Row>
   </div>
@@ -132,29 +142,29 @@
       </a>
       <Column>
         {#if navButtons}
-          {#each navButtons as navButton}
-            {@render navButton()}
+          {#each navButtons as { key, label, variant }}
+            {@render navButton(key, label, variant)}
           {/each}
         {/if}
       </Column>
       <Column>
         {#if navDropdowns}
-          {#each navDropdowns as navDropdown}
-            {@render navDropdown()}
+          {#each navDropdowns as { key, icon, variant, color }}
+            {@render navDropdown(key, icon, variant, color)}
           {/each}
         {/if}
       </Column>
       <div>
-        {#if iconButtons}
-          {#each iconButtons as iconButton}
-            {@render iconButton()}
+        {#if navIconButtons}
+          {#each navIconButtons as { key, icon, variant, color }}
+            {@render navIconButton(key, icon, variant, color)}
           {/each}
         {/if}
       </div>
       <Column class="flex-h gap-sm">
         {#if ctaButtons}
-          {#each ctaButtons as ctaButton}
-            {@render ctaButton()}
+          {#each ctaButtons as { key, label, variant, color }}
+            {@render ctaButton(key, label, variant, color)}
           {/each}
         {/if}
       </Column>
