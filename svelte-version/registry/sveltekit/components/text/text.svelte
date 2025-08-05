2add617c3c092dadff92df7b8ae600ab06bd20e9
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-04 22:11:10 -0700

    fix(render): Fix optional child rendering for text component

--- svelte-version/registry/sveltekit/components/text/text.svelte
+++ svelte-version/registry/sveltekit/components/text/text.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  import { propsToDataAttrs } from "$registry/sveltekit/lib/utilities";
-  import "$registry/sveltekit/components/text/text.css";
+  import { propsToDataAttrs } from "@/registry/sveltekit/lib/utilities";
+  import "@/registry/sveltekit/components/text/text.css";
+	import type { HTMLAttributes } from "svelte/elements";
+	import type { ElementType, JSX } from "react";
+	import type { Snippet } from "svelte";
 
-  type LkSemanticTag = keyof JSX.IntrinsicElements;
+  type LkSemanticTag = JSX.IntrinsicAttributes | Snippet;
 
-  export interface LkTextProps extends React.HTMLAttributes<HTMLElement> {
+  export interface LkTextProps extends HTMLAttributes<HTMLElement> {
     fontClass?: LkFontClass;
     content?: string;
     color?: LkColor;
@@ -12,13 +15,13 @@
     className?: string; // explicitly extracting because internal logic controls its rendering order
   }
 
-  let { tag = "div", fontClass, color, children, style, className, ...restProps }: LkTextProps = $props()
-  const Tag = tag as ElementType;
+  let { tag = "div", fontClass, color, children, style, class: className = "", ...restProps }: LkTextProps = $props()
+  const Tag = tag;
   /**Temporarily removing the attr spreader because it's not being used */
   // const textAttrs = useMemo(() => propsToDataAttrs(restProps, "text"), [restProps]);
 </script>
 
 <Tag data-lk-component="text" className={`${fontClass || ""} ${color ? "color-" + color : ""} ${className || ''}`} style={style} {...restProps}>
-  {@render children()}
+  {@render children?.()}
 </Tag>
 
