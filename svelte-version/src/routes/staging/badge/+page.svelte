ac3ee009f087e578a56ce5c7504d075d82def732
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-07 13:04:59 -0700

    copy(staging): Copy the staging routes over
    This includes the already converted badge page

---
+++ svelte-version/src/routes/staging/badge/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import Row from "@/registry/sveltekit/components/row";
+  import Badge from "@/registry/sveltekit/components/badge";
+
+  const buttonColors: LkColorWithOnToken[] = [
+    "primary",
+    "secondary",
+    "tertiary",
+    "error",
+    "warning",
+    "info",
+    "success",
+    "primarycontainer",
+    "secondarycontainer",
+    "tertiarycontainer",
+    "errorcontainer",
+    "warningcontainer",
+    "infocontainer",
+    "successcontainer",
+    "surface",
+    "surfacecontainerlowest",
+    "surfacecontainerlow",
+    "surfacecontainerhigh",
+    "surfacecontainerhighest",
+    "surfacevariant",
+    "inversesurface",
+    "background",
+  ];
+</script>
+
+<div>
+  <Row>
+    {#each buttonColors as color}
+      <Badge color={color} scale="lg" iconStrokeWidth={1} />
+    {/each}
+  </Row>
+  <Row>
+    {#each buttonColors as color}
+      <Badge color={color}/>
+    {/each}
+  </Row>
+</div>
