8f8b493b93222686b9a2c5c915be3315bbec4857
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-23 20:09:19 -0700

    wip(Button Page): Continue work to migrate component

--- svelte-version/src/routes/staging/button/+page.svelte
+++ svelte-version/src/routes/staging/button/+page.svelte
@@ -5,6 +5,43 @@ import Heading from "$components/heading";
 import Grid from "$components/grid";
 import Row from "$components/row";
 import ThemeController from "$components/theme-controller";
+import { IconName } from "lucide-react/dynamic";
+import type { Snippet } from "svelte";
+
+  type ButtonSnippetProps = {
+    colors: LkColorWithOnToken[];
+    colorIndex: number;
+    variants: string[];
+    variantIndex: number;
+    sizes: typeof sizes;
+    sizeIndex: number;
+    iconIndex: number;
+    currentConfig: (typeof iconConfigs)[number];
+	};
+
+  const sizes = ['sm', 'md', 'lg'] as const;
+  
+	const iconConfigs = [
+		{ startIcon: undefined, endIcon: undefined, label: 'No Icons' },
+		{ startIcon: 'circle', endIcon: undefined, label: 'Start Icon' },
+		{ startIcon: undefined, endIcon: 'circle', label: 'End Icon' },
+		{ startIcon: 'circle', endIcon: 'circle', label: 'Both Icons' }
+	] as const;
+
+	const scaleFactors: LkFontClass[] = [
+		'display1',
+		'display2',
+		'title1',
+		'title2',
+		'title3',
+		'heading',
+		'subheading',
+		'body',
+		'callout',
+		'label',
+		'caption',
+		'capline'
+	] as const;
 
   const buttonColors: LkColorWithOnToken[] = [
     "primary",
@@ -29,10 +66,30 @@ import ThemeController from "$components/theme-controller";
     "surfacevariant",
     "inversesurface",
     "background",
-  ];
-
-  const buttonVariants = ["fill", "outline", "text"];
+  ] as const;
 </script>
+
+{#snippet button({
+	colors,
+	colorIndex,
+	variants,
+	variantIndex,
+	sizes,
+	sizeIndex,
+	iconIndex,
+	currentConfig
+}: ButtonSnippetProps)}
+	<Button
+		label={`Button`}
+		variant={variants[variantIndex] as 'fill' | 'outline' | 'text'}
+		color={colors[colorIndex] as LkColorWithOnToken}
+		size={sizes[sizeIndex] as 'sm' | 'md' | 'lg'}
+		startIcon={currentConfig.startIcon as IconName | undefined}
+		endIcon={currentConfig.endIcon as IconName | undefined}
+		opticIconShift={false}
+		style={'alignSelf: "center"; justifySelf: "center";'}
+	/>
+{/snippet}
 
 <div>
   <Section>
@@ -41,16 +98,9 @@ import ThemeController from "$components/theme-controller";
       Hello world!
     </Heading>
   </Section>
-  <Grid columns={4} gap="md" style={{alignItems: "center", justifyItems: "center"}}>
+  <Grid columns={4} gap="md" style='alignItems: "center"; justifyItems: "center";'>
 
     {(() => {
-      const sizes = ["sm", "md", "lg"];
-      const iconConfigs = [
-        { startIcon: undefined, endIcon: undefined, label: "No Icons" },
-        { startIcon: "circle", endIcon: undefined, label: "Start Icon" },
-        { startIcon: undefined, endIcon: "circle", label: "End Icon" },
-        { startIcon: "circle", endIcon: "circle", label: "Both Icons" },
-      ];
 
       const generateButtons = (
         colors: LkColorWithOnToken[],
@@ -61,19 +111,20 @@ import ThemeController from "$components/theme-controller";
         variantIndex = 0,
         sizeIndex = 0,
         iconIndex = 0
-      ): React.JSX.Element[] => {
+      ) => {
+        // Return an empty array if the index exceeds the array range
         if (colorIndex >= colors.length) return [];
+        // If variant index exceeds possible variants
         if (variantIndex >= variants.length)
           return generateButtons(colors, variants, sizes, iconConfigs, colorIndex + 1, 0, 0, 0);
+        // If size index exceeds possible sizes
         if (sizeIndex >= sizes.length)
           return generateButtons(colors, variants, sizes, iconConfigs, colorIndex, variantIndex + 1, 0, 0);
+        // If iconIndex exceeds 
         if (iconIndex >= iconConfigs.length)
           return generateButtons(colors, variants, sizes, iconConfigs, colorIndex, variantIndex, sizeIndex + 1, 0);
 
         const currentConfig = iconConfigs[iconIndex];
-        const button = (
-          
-        );
         
         return [
           button,
@@ -94,5 +145,5 @@ import ThemeController from "$components/theme-controller";
     })()}
     
   </Grid>
-  <Row alignItems="start" style={{ gap: "var(--lk-size-md)" }} className="w-full" wrapChildren={true}></Row>
+  <Row alignItems="start" style='gap: "var(--lk-size-md)";' class="w-full" wrapChildren={true}></Row>
 </div>
