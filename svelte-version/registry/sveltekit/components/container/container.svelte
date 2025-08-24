553a5eff72d52ca2f245921447b5eaf83dff4092
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-08-23 20:07:41 -0700

    update(container): Add optional child rendering

--- svelte-version/registry/sveltekit/components/container/container.svelte
+++ svelte-version/registry/sveltekit/components/container/container.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
   import { propsToDataAttrs } from "@/registry/sveltekit/lib/utilities";
   import "@/registry/sveltekit/components/container/container.css";
+	import type { HTMLAttributes } from "svelte/elements";
 
-  interface LkContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  interface LkContainerProps extends HTMLAttributes<HTMLDivElement> {
     maxWidth?: LkContainerWidth;
   }
 
   let props: LkContainerProps = $props();
-    const { maxWidth = "md", children, ...restProps } = props;
+  const { maxWidth = "md", children, ...restProps } = props;
     
-    const dataAttrs = useMemo(
-      () => propsToDataAttrs({ maxWidth }, "container"),
-      [maxWidth],
-    );
+  const dataAttrs = $derived(
+    propsToDataAttrs({ maxWidth }, "container"),
+  );
 </script>
 
 <div {...dataAttrs} {...restProps}>
-  {@render children()}
+  {@render children?.()}
 </div>
