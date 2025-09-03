f0bab1b259a4fc059c53bc1d12e117e7aa60e6bb
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-09-02 19:04:38 -0700

    update(root layout): Bring inline closer with nextjs version

--- svelte-version/src/routes/+layout.svelte
+++ svelte-version/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '../globals.css';
+	import ThemeProvider from "$components/theme"
 	let { children } = $props();
 </script>
 
-{@render children?.()}
+<ThemeProvider>
+	{@render children?.()}
+</ThemeProvider>
+
+<style>
+	/* Google Inter Font Download */
+	@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+</style>
